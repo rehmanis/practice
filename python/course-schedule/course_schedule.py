90753b9bf5ec6a715315a9b89c404844a95ec274
Shamsuddin Rehmani <rehmani@usc.edu>
2021-04-08 21:26:58 -0400

    TEST: add basic tests for course schedule problem

--- python/course-schedule/course_schedule.py
+++ python/course-schedule/course_schedule.py
@@ -1,51 +1,53 @@
 import sys
-from typing import List  # Python 3.8 and earlier
+from typing import List
+
 
 class Solution:
-    
+
     def compute_adj_list(self, edges: List[int], nodes):
         adj = {}
-        
+
         for node in range(nodes):
             adj[node] = []
-            
+
         for edge in edges:
             adj[edge[0]].append(edge[1])
-            
+
         return adj
-        
-    
+
     def has_cycle(self, adj, v, visited, onstack):
         visited[v] = True
         onstack[v] = True
-        
+
         for neighbor in adj[v]:
-            
-            if not visited[neighbor] and self.has_cycle(adj, neighbor, visited, onstack):
-                # res = self.has_cycle(adj, neighbor, visited, onstack)
-                # if res:
+
+            if (
+                not visited[neighbor] and
+                self.has_cycle(adj, neighbor, visited, onstack)
+            ):
                 return True
             elif onstack[neighbor]:
                 return True
-        
+
         onstack[v] = False
-        
+
         return False
-        
-    
-    def can_finish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
+
+    def can_finish(
+        self, numCourses: int, prerequisites: List[List[int]]
+    ) -> bool:
+
         adj = self.compute_adj_list(prerequisites, numCourses)
         visited = [False] * numCourses
         onstack = [False] * numCourses
-        
+
         for course in range(numCourses):
-            
+
             is_cycle = self.has_cycle(adj, course, visited, onstack)
-            
+
             if is_cycle:
                 return False
-        
+
         return True
 
 
